e411f0f4924e67bdb488517cf675653b8f050e93
Pedro Pais <pgpaisdev@gmail.com>
2022-07-12 14:04:50 +0100

    still working out how to get strokes for each trial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,16 @@
 import lemon.TrainingSession;
+import lemon.Trial;
 import pt.up.hs.uhc.UniversalHandwritingConverter;
-import pt.up.hs.uhc.models.Format;
 import pt.up.hs.uhc.models.Page;
 import pt.up.hs.uhc.models.Stroke;
 
 import java.io.*;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class Main {
+    private static final float MIN_NAME_TO_CHARACTER_DELAY = 60 * 1000; // in milliseconds
+    private static final float MIN_DELAY_BETWEEN_TRAININGS = 600 * 1000; // in milliseconds
+    private static final float MIN_DELAY_BETWEEN_TRIALS = 1 * 1000; // in milliseconds
 
     public static void main(String[] args) {
         String filePath = "log.neonotes";
@@ -31,19 +33,65 @@ public class Main {
         File file = new File(filePath);
         System.out.println(file.getAbsolutePath());
         converter.file(file);
-
         List<Page> pages = converter.getPages();
-        Page page = pages.get(0);
-        long firstTimestamp = page.getStrokes().get(0).getStartTime();
-        Date firstDate = new Date(firstTimestamp);
-        Date lastDate = new Date(firstTimestamp);
-        for (Stroke stroke : page.getStrokes()) {
-            Date curDate = new Date(stroke.getStartTime());
-            System.out.println("Absolute Timestamp: " + stroke.getStartTime() + " | Relative Timestamp: " + (stroke.getStartTime() - firstTimestamp));
-            System.out.println("Timestamp date: " + curDate.toString());
-            System.out.println("Seconds before last stroke: " + ((curDate.getTime() - lastDate.getTime())/1000.0));
-            System.out.println();
-            lastDate = curDate;
+        // Getting 2nd page for now
+        Page page = pages.get(1);
+
+        TrainingSession trainingSession = new TrainingSession();
+
+        // Look for all strokes, look for a specific delay (e.g. 1 second) as that is the time between the name and the character drawings
+        List<Stroke> nameStrokes = new ArrayList<>();
+        List<Stroke> characterStrokes = new ArrayList<>();
+        boolean foundFinalNameStroke = false;
+        Date lastStrokeDate = null;
+        for (Stroke stroke :
+                page.getStrokes()) {
+            Date currentStrokeDate = new Date(stroke.getStartTime());
+            System.out.println(currentStrokeDate.toString());
+
+            if (lastStrokeDate == null) {
+                nameStrokes.add(stroke);
+
+            } else {
+                boolean isAnotherTraining = (currentStrokeDate.getTime() - lastStrokeDate.getTime()) > MIN_DELAY_BETWEEN_TRAININGS;
+                boolean isNotNameStroke = (currentStrokeDate.getTime() - lastStrokeDate.getTime()) > MIN_NAME_TO_CHARACTER_DELAY;
+                if(isAnotherTraining){
+                    break;
+                }
+                else if (isNotNameStroke || foundFinalNameStroke) {
+                    foundFinalNameStroke = true;
+                    characterStrokes.add(stroke);
+                    //TODO: parse strokes to find if they are part of same trial and, if not, create a new one and add to training
+                    //use the constant
+                    boolean isNotInSameTrial = (currentStrokeDate.getTime() - lastStrokeDate.getTime()) > MIN_DELAY_BETWEEN_TRIALS;
+                    if(isNotInSameTrial){
+                        Trial trial = new Trial(characterStrokes);
+                        trainingSession.addTrial(trial);
+                        characterStrokes = new ArrayList<>();
+                    }else{
+                        characterStrokes.add(stroke);
+                    }
+                }
+                else {
+                    nameStrokes.add(stroke);
+                }
+            }
+            lastStrokeDate = new Date(stroke.getEndTime());
         }
+        trainingSession.addNameStrokes(nameStrokes);
+        trainingSession.addCharacterStrokes(characterStrokes);
+
+//        Page page = pages.get(0);
+//        long firstTimestamp = page.getStrokes().get(0).getStartTime();
+//        Date firstDate = new Date(firstTimestamp);
+//        Date lastDate = new Date(firstTimestamp);
+//        for (Stroke stroke : page.getStrokes()) {
+//            Date curDate = new Date(stroke.getStartTime());
+//            System.out.println("Absolute Timestamp: " + stroke.getStartTime() + " | Relative Timestamp: " + (stroke.getStartTime() - firstTimestamp));
+//            System.out.println("Timestamp date: " + curDate.toString());
+//            System.out.println("Seconds before last stroke: " + ((curDate.getTime() - lastDate.getTime())/1000.0));
+//            System.out.println();
+//            lastDate = curDate;
+//        }
     }
 }
--- src/main/java/lemon/TrainingSession.java
+++ src/main/java/lemon/TrainingSession.java
@@ -1,33 +1,42 @@
 package lemon;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import pt.up.hs.uhc.models.Stroke;
+
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class TrainingSession {
-    private final List<CharacterRound> characterRounds;
-    private final String timestamp;
 
     private Date startTime;
+    private Date endTime;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss.nnnnnnnnn").withZone(ZoneId.systemDefault());
 
-    public TrainingSession(int numberOfRounds, String timestamp){
-        characterRounds = new ArrayList<>(numberOfRounds);
-        this.timestamp = timestamp;
+    private List<Stroke> nameStrokes;
+    private double nameWriteDuration;
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss.SSS");
-        try {
-            startTime = sdf.parse(timestamp);
-            System.out.println(startTime.toString());
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+    private List<Trial> trials;
+
+    public TrainingSession(){
+        trials = new ArrayList<>();
+    }
+    private List<Stroke> characterStrokes;
+
+    public void addNameStrokes(List<Stroke> strokes){
+        this.nameStrokes = strokes;
+
+        Date nameStartDate = new Date(strokes.get(0).getStartTime());
+        Date nameEndDate = new Date(strokes.get(strokes.size()-1).getEndTime());
+        nameWriteDuration = (nameEndDate.getTime() - nameStartDate.getTime()) / 1000.0;
     }
 
-    public CharacterRound createCharacterRound(int characterIndex, String characterRoundStartTimestamp){
-        CharacterRound characterRound = new CharacterRound(characterIndex, characterRoundStartTimestamp);
-        characterRounds.add(characterRound);
-        return characterRound;
+    public void addCharacterStrokes(List<Stroke> characterStrokes) {
+        this.characterStrokes = characterStrokes;
+    }
+
+    public void addTrial(Trial trial){
+        trials.add(trial);
     }
 }
